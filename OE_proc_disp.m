f3105253037a5039207bf0b6c1963ac82af85011
vncntprvst <vp35@gmail.com>
2015-09-11 17:45:31 -0400

    processing OE spike data
    one channel at a time

--- OE_proc_disp.m
+++ OE_proc_disp.m
@@ -1,19 +1,121 @@
 %% Process and display data from Open-Ephys recordings  
 
-cd C:\Data\OpenEphys\PrV25_2015-03-09_20-12-55_test2
+%% Get file path
 
+cd C:\Data\OpenEphys\PrV25_12_train2_2015-09-10_20-26-30
+
+%% Get Spike data
 %O_E binary format - see also 'load_open_ephys_data.m'
 % fid = fopen('102_CH1.continuous'); 
-fid = fopen('all_channels.events');
-hdr = fread(fid, 1024, 'char*1');
-timestamp = fread(fid, 1, 'int64',0,'l');
-N = fread(fid, 1, 'uint16',0,'l');
-recordingNumber = fread(fid, 1, 'uint16', 0, 'l');
-samples = fread(fid, N, 'int16',0,'b');
-recordmarker = fread(fid, 10, 'char*1');
-fclose(fid);
-figure; plot(samples);
+% fid = fopen('all_channels.events');
+% hdr = fread(fid, 1024, 'char*1');
+% timestamp = fread(fid, 1, 'int64',0,'l');
+% N = fread(fid, 1, 'uint16',0,'l');
+% recordingNumber = fread(fid, 1, 'uint16', 0, 'l');
+% samples = fread(fid, N, 'int16',0,'b');
+% recordmarker = fread(fid, 10, 'char*1');
+% fclose(fid);
 
 %kwik data
-h5disp('experiment1.kwik','/event_types/TTL')
-data = h5read('experiment1.kwik','/event_types/TTL/events/recording')
+% KWIK contains metadata about the experiment, as well as event data
+% h5disp('experiment1.kwik','/recordings/');
+SamplingRate=h5readatt('experiment1.kwik','/recordings/0','sample_rate');
+% KWX contains the spike data
+% h5disp('experiment1.kwx')
+ChanInfo=h5info('experiment1.kwx');
+
+% get data from one channel
+Chan(5).Units=h5read('experiment1.kwx','/channel_groups/4/recordings');
+Chan(5).SpikeTimes=h5read('experiment1.kwx','/channel_groups/4/time_samples');
+% Chan(6).SpikeTimes=h5read('experiment1.kwx','/channel_groups/5/time_samples');
+Chan(5).Waveforms=h5read('experiment1.kwx','/channel_groups/4/waveforms_filtered');
+% figure; plot(Chan(5).SpikeTimes);
+
+%KWD contains the continuous data for a given processor in the /recordings/#/data dataset, where # is the recording number, starting at 0
+
+
+%% get Trial structure
+% h5disp('experiment1.kwik','/event_types/TTL')
+% TTLinfo=h5info('experiment1.kwik','/event_types/TTL');
+
+TTL_ID = h5read('experiment1.kwik','/event_types/TTL/events/user_data/eventID');
+TTL_times = h5read('experiment1.kwik','/event_types/TTL/events/time_samples');
+% keep absolute time of TTL onset
+TTL_times=TTL_times(diff([0;TTL_ID])>0); 
+% TTL sequence
+TTL_seq=diff(TTL_times); 
+%we send 10ms TTLs, with 10ms interval. Two pulses for begining of trial 
+%(e.g.,head through front panel). With sampling rate of 30kHz, that 
+% interval should be 601 samples (20ms*30+1). Or 602 accounting for jitter.
+% TTL_seq should thus read as: 
+%   601
+%   end of trial time
+%   inter-trial interval
+if TTL_seq(1)>=610 %missed first trial initiation, discard times
+    TTL_seq(1)=999;
+end
+if TTL_seq(end)<=610 %unfinished last trial
+    TTL_seq(end)=999;
+end
+    
+Trials.start=TTL_times([TTL_seq<=610;false]);%Trials.start=Trials.start./uint64(SamplingRate/1000)
+Trials.end=TTL_times(find([TTL_seq<=610;false])+2);
+Trials.interval=TTL_times(find([TTL_seq<=610;false])+3)-TTL_times(find([TTL_seq<=610;false])+2);
+
+%% gather data
+Spkt=Chan(5).SpikeTimes;
+Rasters{1}=zeros(size(Trials.start,1),2000); %begining of trial
+Rasters{2}=zeros(size(Trials.end,1),2000); %end of trial
+for trialnb=1:size(Trials.start,1)
+%begining of trial
+RastSWin=Trials.start(trialnb)-uint64(SamplingRate); % 1 sec before
+RastEWin=Trials.start(trialnb)+uint64(SamplingRate); % 1 sec afer
+SpikeTimes=round((Spkt(Spkt>RastSWin & Spkt<RastEWin)-RastSWin)/uint64(SamplingRate/1000));
+SpikeTimes(SpikeTimes==0)=1; %no 0 indices
+SpikeTimes=unique(SpikeTimes);
+Rasters{1}(trialnb,SpikeTimes)=1;
+%end of trial
+RastSWin=Trials.end(trialnb)-uint64(SamplingRate); % 1 sec before
+RastEWin=Trials.end(trialnb)+uint64(SamplingRate); % 1 sec afer
+SpikeTimes=round((Spkt(Spkt>RastSWin & Spkt<RastEWin)-RastSWin)/uint64(SamplingRate/1000));
+SpikeTimes(SpikeTimes==0)=1; %no 0 indices
+SpikeTimes=unique(SpikeTimes);
+Rasters{2}(trialnb,SpikeTimes)=1;
+end
+
+%% plot raster
+
+%% plot sdf
+start=1;
+stop=size(Rasters{1},2);
+conv_sigma=20;
+alignmtt=1000;
+[sdf{1}, ~, rastsem{1}]=conv_raster(Rasters{1},conv_sigma,start,stop);
+[sdf{2}, ~, rastsem{2}]=conv_raster(Rasters{2},conv_sigma,start,stop);
+figure;
+colormap default;
+cmap = colormap(gcf);
+hold on;
+
+%plot sem
+patch([1:length(sdf{1}),fliplr(1:length(sdf{1}))],[sdf{1}-rastsem{1},fliplr(sdf{1}+rastsem{1})],cmap(1,:),'EdgeColor','none','FaceAlpha',0.1);
+patch([1:length(sdf{2}),fliplr(1:length(sdf{2}))],[sdf{2}-rastsem{2},fliplr(sdf{2}+rastsem{2})],cmap(22,:),'EdgeColor','none','FaceAlpha',0.1);
+%plot sdf
+plot(sdf{1},'Color',cmap(1,:),'LineWidth',1.8);
+plot(sdf{2},'Color',cmap(22,:),'LineWidth',1.8);
+
+set(gca,'XTick',[0:100:(stop-start-6*conv_sigma)]);
+set(gca,'XTickLabel',-(alignmtt-(start+3*conv_sigma)):100:stop-(alignmtt+3*conv_sigma));
+axis(gca,'tight'); box off;
+set(gca,'Color','white','TickDir','out','FontName','Cambria','FontSize',10);
+hxlabel=xlabel(gca,'Time (ms)','FontName','Cambria','FontSize',10);
+hylabel=ylabel(gca,'Firing rate (spikes/s)','FontName','Cambria','FontSize',10);
+
+% draw alignment bar
+currylim=get(gca,'YLim');
+patch([repmat((alignmtt-(start+3*conv_sigma))-2,1,2) repmat((alignmtt-(start+3*conv_sigma))+2,1,2)], ...
+    [[0 currylim(2)] fliplr([0 currylim(2)])], ...
+    [0 0 0 0],[1 0 0],'EdgeColor','none','FaceAlpha',0.5);
+
+%legend
+legend('Front panel exploration','Reward port');

9a4cf082c88f4ec41f8b15ef5ab4fb2e060ebb1a
Vincent <vp35@duke.edu>
2020-07-22 11:39:17 -0400

    fixes audio

--- WhiskingBoutAudio.m
+++ WhiskingBoutAudio.m
@@ -1,13 +1,19 @@
-function wBoutAudio=WhiskingBoutAudio(ephys,unitNum,boutIndex,factor)
+function wBoutAudio=WhiskingBoutAudio(rasters,sampleRate,factor)
 
 %% Add spike audio
 % FacePro.MakeSpikeAudio()
-sampleRate=1000; %factor=20;
-spikeTimes = find(ephys.rasters(unitNum,boutIndex))/sampleRate;
+% sampleRate=1000; %factor=20;
+spikeTimes = find(rasters)/sampleRate;
 % waveforms = sortedSpikeWaveforms;
-waveforms = repmat(mean([[gausswin(6); zeros(2,1)],[zeros(2,1);-gausswin(6)]],2), 1, sum(ephys.rasters(unitNum,boutIndex))); %gausswin(8)
+adjFact=factor/10;
+% waveforms = repmat(mean([[gausswin(6); zeros(2,1)],...
+%     [zeros(2,1);-gausswin(6)]],2), 1, sum(rasters(:,boutIndex))); %gausswin(8)
+% waveforms = repmat(mean([[gausswin(6*adjFact); zeros(2*adjFact,1)],...
+%     [zeros(2*adjFact,1);-gausswin(6*adjFact)]],2), 1, sum(rasters(:,boutIndex))); %gausswin(8)
+waveforms = repmat(gausswin(8*adjFact), 1, sum(rasters)); 
+
 spikeTimesScaled = round(spikeTimes * factor * sampleRate);
-wBoutAudio = zeros(length(boutIndex)*factor, 1);
+wBoutAudio = zeros(numel(rasters)*factor, 1);
 %             else
 %                 spikeBinary = sweep(:);
 %                 waveforms = repmat(-gausswin(8), 1, sum(spikeBinary));
@@ -15,11 +21,40 @@ wBoutAudio = zeros(length(boutIndex)*factor, 1);
 %                 aud = zeros(length(spikeBinary)*factor, 1);
 %             end
 
-for j = 1 : size(waveforms,2)
-    wBoutAudio(spikeTimesScaled(j):spikeTimesScaled(j)+size(waveforms,1)-1) = waveforms(:,j);
+for spikeNum = 1 : size(waveforms,2)
+    wBoutAudio(spikeTimesScaled(spikeNum):spikeTimesScaled(spikeNum)+size(waveforms,1)-1) = ...
+        wBoutAudio(spikeTimesScaled(spikeNum):spikeTimesScaled(spikeNum)+size(waveforms,1)-1) + waveforms(:,spikeNum);
 end
 
-wBoutAudio = interp1(1:length(wBoutAudio),wBoutAudio,1:sampleRate*(length(boutIndex)/1000)*factor);
-wBoutAudio = reshape(wBoutAudio, [ round(length(wBoutAudio)/length(boutIndex)), length(boutIndex) ]);
+% wBoutAudio = interp1(1:length(wBoutAudio),wBoutAudio,1:sampleRate*(numel(rasters)/sampleRate)*factor);
+
+wBoutAudio = reshape(wBoutAudio, [ round(length(wBoutAudio)/numel(rasters)), numel(rasters) ]);
+
 % wBoutAudio = wBoutAudio(:, frameRange(1):frameRange(2));
 wBoutAudio = int16(wBoutAudio / max(abs(wBoutAudio(:))) * double(intmax('int16')));
+% 
+%             aud = interp1(1:length(aud),aud,1:sampleRate*5*factor);
+%             aud = reshape(aud, [ round(length(aud)/2500), 2500 ]);
+%             aud = aud(:, frameRange(1):frameRange(2));
+%             aud = int16(aud / max(abs(aud(:))) * double(intmax('int16')));
+% 
+% 
+%             aud = FacePro.MakeSpikeAudio(rasters(boutIndex), 10, [1 2500], sampleRate);
+%             
+% %             
+% soundsc(wBoutAudio',sampleRate*factor/20)
+% soundsc(double(vertcat(wBoutAudio(:))));
+% soundsc(waveforms(:,1),sampleRate/2*10) 
+% soundsc(gausswin(8),sampleRate/2*10) 
+% soundsc(double(vertcat(aud(:))))
+% 
+% foo=rasters(unitNum,boutIndex);
+% figure; hold on;
+% imagesc(wBoutAudio)
+% % plot(rasters(unitNum,boutIndex))
+% plot(spikeTimes*sampleRate+0.5,ones(numel(spikeTimes),1),'kd')
+% 
+% figure; imagesc(waveforms)
+% figure; imagesc(wBoutAudio)
+% figure; imagesc(aud)
+
